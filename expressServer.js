7651585bc6504aa92bbde8fb3eb27315c951945d
Anindya-Pandey <anindyapandey@gmail.com>
2019-07-25 11:59:51 +0530

    check get request

--- expressServer.js
+++ expressServer.js
@@ -5,6 +5,11 @@ let app = express();
 
 app.use(bodyParser.json());
 
+app.get("/", function(req, res, next){
+	res.send("Helooo");
+	res.end();
+});
+
 app.listen("3000", function(){
 	console.log("Listening on 3000");
 });
